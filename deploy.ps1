24844f300449d6f2417697306c7a8700d27016e3
Mihoshi <74931190+Srakawichi@users.noreply.github.com>
2025-04-30 14:54:52 +0200

    improved powerShell skript

--- deploy.ps1
+++ deploy.ps1
@@ -3,11 +3,13 @@ $NAO_IP = "169.254.14.127"
 $NAO_USER = "nao"
 $LOCAL_SCRIPT_PATH = "scripts\hello.py"
 $REMOTE_SCRIPT_NAME = [System.IO.Path]::GetFileName($LOCAL_SCRIPT_PATH)
+$REMOTE_PATH = "/home/nao/$REMOTE_SCRIPT_NAME"
+$REMOTE_COMMAND = "/home/nao/.local/share/PackageManager/apps/python3-nao/bin/python3 $REMOTE_PATH"
 
 # === Übertragung ===
-Write-Host "📤 Übertrage $LOCAL_SCRIPT_PATH → $NAO_USER@$NAO_IP:/home/nao/$REMOTE_SCRIPT_NAME"
-scp $LOCAL_SCRIPT_PATH "$NAO_USER@$NAO_IP:/home/nao/$REMOTE_SCRIPT_NAME"
+Write-Host ">> Übertrage $LOCAL_SCRIPT_PATH -> ${NAO_USER}@${NAO_IP}:${REMOTE_PATH}"
+scp $LOCAL_SCRIPT_PATH "${NAO_USER}@${NAO_IP}:${REMOTE_PATH}"
 
 # === Ausführung (mit Loganzeige) ===
-Write-Host "🚀 Starte /home/nao/$REMOTE_SCRIPT_NAME auf dem NAO"
-ssh $NAO_USER@$NAO_IP "/home/nao/.local/share/PackageManager/apps/python3-nao/bin/python3 /home/nao/$REMOTE_SCRIPT_NAME"
+Write-Host ">> Starte ${REMOTE_PATH} auf dem NAO"
+& ssh "${NAO_USER}@${NAO_IP}" $REMOTE_COMMAND
