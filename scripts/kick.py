2e2a55229f760d25f7669ffb1e376906ea9a2c8b
lukas <82029186+42lukas@users.noreply.github.com>
2025-06-11 14:48:38 +0200

    kick minor changes

--- scripts/kick.py
+++ scripts/kick.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import time
-import wake_up
 
 FRAME_TORSO = 0
 AXIS_MASK_ALL = 63
@@ -8,8 +7,6 @@ AXIS_MASK_ALL = 63
 def kick(session, leg):
     motion_service = session.service("ALMotion")
     posture_service = session.service("ALRobotPosture")
-
-    wake_up.wake_up(session)
 
     # balance preparation
     effector_torso = "Torso"
@@ -19,10 +16,10 @@ def kick(session, leg):
 
     if leg.lower() == "left":
         effector_leg = "LLeg"
-        target[1] -= 0.0058
+        target[1] -= 0.0054
     elif leg.lower() == "right":
         effector_leg = "RLeg"
-        target[1] += 0.0058
+        target[1] += 0.0054
     else:
         raise ValueError("Ungueltiges Bein. Verwende 'left' oder 'right'.")
 
@@ -37,7 +34,7 @@ def kick(session, leg):
 
     # kick
     path = [
-        [0.0, 0.0, 0.03, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
         [0.05, 0.0, 0.03, 0.0, 0.0, 0.0],
         [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
     ]
