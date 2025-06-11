accfac27ffc8f53190cdc29f7048497cf2af49e5
lukas <82029186+42lukas@users.noreply.github.com>
2025-06-11 14:28:23 +0200

    minor changes

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,15 @@
 import qi
 from kick import kick
 from arms_behind_back import arms_behind_back
+from wake_up import wake_up
 
 session = qi.Session()
 session.connect("tcp://192.168.200.52:9559")   # NAO-IP und Port
 
+# NAO aufwecken:
+wake_up(session)
+
+# Arme hinter den Rücken:
 arms_behind_back(session)
 
 # Rechter Schuss:
